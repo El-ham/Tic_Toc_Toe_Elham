fd096f1efaabf85537cb56589cef23c7d3cd96da
Elham Afruzi <e.nabipoor@gmail.com>
2023-08-04 15:56:57 -0700

    Tic_Toc_Toe_game_by_Elham

---
+++ Package_Tic_Toc_Toe_Elham/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup
+
+setup(name='Tic_Toc_Toe_Elham',
+      version='0.1',
+      description='Tic_Toc_Toe_game',
+      packages=['Tic_Toc_Toe_Elham'],
+      zip_safe=False)
+
